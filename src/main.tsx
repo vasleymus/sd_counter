8ccc9a9e3688c1e3ca2f900c3fbac4295e1a56b0
Vasley <vasleymus@gmail.com>
2024-10-24 23:35:55 +0400

    Configure msw and mock '/api/count' endpoint

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+export async function enableMocking() {
+  // enabled only in development
+  if (!import.meta.env.DEV) return;
+
+  const { worker } = await import("./mocks/browser.ts");
+
+  return worker.start();
+}
+
+enableMocking()
+  .then(() => {
+    createRoot(document.getElementById("root")!).render(
+      <StrictMode>
+        <App />
+      </StrictMode>
+    );
+  })
+  .catch((err) => {
+    console.error("Failed to enable mocking:", err);
+  });
