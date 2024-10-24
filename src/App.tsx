3ead59cb303e88cfb6ae9b8c47604afba39ac55c
Vasley <vasleymus@gmail.com>
2024-10-24 23:26:43 +0400

    Implement Counter component according to test cases

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { useState } from "react";
 import "./App.css";
 
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount((c) => c + 1);
+  };
+
+  const handleDecrement = () => {
+    setCount((c) => c - 1);
+  };
+
+  return (
+    <div>
+      <h1>The count is: {count}</h1>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
+    </div>
+  );
+}
+
 function App() {
-  return <h1>Initial Application State</h1>
+  return <Counter />;
 }
 
 export default App;
