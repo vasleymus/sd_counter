274756f0f275ffbc5a7d293c5c6f6f0e7e8f4c97
Vasley <vasleymus@gmail.com>
2024-10-25 11:24:47 +0400

    Add Stage B test cases

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,98 +1,130 @@
-import { beforeEach, describe, it, expect } from "vitest";
-import { render, screen, cleanup } from "@testing-library/react";
+import { beforeAll, afterAll, beforeEach, describe, it, expect } from "vitest";
+import { render, screen, cleanup, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom/vitest";
 import userEvent from "@testing-library/user-event";
 import App from "../src/App";
+import { server } from "./mocks/node";
+import { INITIAL_COUNT } from "./mocks/handlers";
 
-describe("Stage A", () => {
+describe("Stage B", () => {
+  beforeAll(() => server.listen());
+  afterAll(() => server.close());
+
   // ensure a clean state for each test
   beforeEach(() => {
+    server.resetHandlers();
     cleanup();
     render(<App />);
   });
 
-  it("should render App component with the count of 0", async () => {
-    const heading = screen.getByRole("heading");
-    expect(heading).toBeInTheDocument();
-    expect(heading).toHaveTextContent("The count is: 0");
+  it("should render App component with the initial count", async () => {
+    await waitFor(() => {
+      const heading = screen.getByRole("heading");
+      expect(heading).toBeInTheDocument();
+      expect(heading).toHaveTextContent(`The count is: ${INITIAL_COUNT}`);
+    });
   });
 
   it("should render Increment button", async () => {
-    const button = screen.getByRole("button", { name: "Increment" });
-    expect(button).toBeInTheDocument();
+    await waitFor(() => {
+      const button = screen.getByRole("button", { name: "Increment" });
+      expect(button).toBeInTheDocument();
+    });
   });
 
   it("should render Decrement button", async () => {
-    const button = screen.getByRole("button", { name: "Decrement" });
-    expect(button).toBeInTheDocument();
+    await waitFor(() => {
+      const button = screen.getByRole("button", { name: "Decrement" });
+      expect(button).toBeInTheDocument();
+    });
   });
 
   it("should increment the count", async () => {
-    const incrementButton = screen.getByRole("button", { name: "Increment" });
-    expect(incrementButton).toBeInTheDocument();
+    const result = INITIAL_COUNT + 1;
 
-    const user = userEvent.setup();
-    await user.click(incrementButton);
+    await waitFor(async () => {
+      const incrementButton = screen.getByRole("button", { name: "Increment" });
+      expect(incrementButton).toBeInTheDocument();
 
-    const heading = screen.getByRole("heading");
-    expect(heading).toHaveTextContent("The count is: 1");
+      const user = userEvent.setup();
+      await user.click(incrementButton);
+
+      const heading = screen.getByRole("heading");
+      expect(heading.textContent).toBe(`The count is: ${result}`);
+    });
   });
 
   it("should increment the count 3 times", async () => {
-    const incrementButton = screen.getByRole("button", { name: "Increment" });
-    expect(incrementButton).toBeInTheDocument();
+    const result = INITIAL_COUNT + 3;
 
-    const user = userEvent.setup();
-    await user.click(incrementButton);
-    await user.dblClick(incrementButton);
+    await waitFor(async () => {
+      const incrementButton = screen.getByRole("button", { name: "Increment" });
+      expect(incrementButton).toBeInTheDocument();
 
-    const heading = screen.getByRole("heading");
-    expect(heading).toHaveTextContent("The count is: 3");
+      const user = userEvent.setup();
+      await user.click(incrementButton);
+      await user.dblClick(incrementButton);
+
+      const heading = screen.getByRole("heading");
+      expect(heading.textContent).toBe(`The count is: ${result}`);
+    });
   });
 
   it("should decrement the count", async () => {
-    const decrementButton = screen.getByRole("button", { name: "Decrement" });
-    expect(decrementButton).toBeInTheDocument();
+    const result = INITIAL_COUNT - 1;
 
-    const user = userEvent.setup();
-    await user.click(decrementButton);
+    await waitFor(async () => {
+      const decrementButton = screen.getByRole("button", { name: "Decrement" });
+      expect(decrementButton).toBeInTheDocument();
 
-    const heading = screen.getByRole("heading");
-    expect(heading).toHaveTextContent("The count is: -1");
+      const user = userEvent.setup();
+      await user.click(decrementButton);
+
+      const heading = screen.getByRole("heading");
+      expect(heading).toHaveTextContent(`The count is: ${result}`);
+    });
   });
 
   it("should decrement the count 3 times", async () => {
-    const decrementButton = screen.getByRole("button", { name: "Decrement" });
-    expect(decrementButton).toBeInTheDocument();
+    const result = INITIAL_COUNT - 3;
 
-    const user = userEvent.setup();
-    await user.click(decrementButton);
-    await user.dblClick(decrementButton);
+    await waitFor(async () => {
+      const decrementButton = screen.getByRole("button", { name: "Decrement" });
+      expect(decrementButton).toBeInTheDocument();
 
-    const heading = screen.getByRole("heading");
-    expect(heading).toHaveTextContent("The count is: -3");
+      const user = userEvent.setup();
+      await user.click(decrementButton);
+      await user.dblClick(decrementButton);
+
+      const heading = screen.getByRole("heading");
+      expect(heading).toHaveTextContent(`The count is: ${result}`);
+    });
   });
 
   it("should increment and decrement the count a few times", async () => {
-    const incrementButton = screen.getByRole("button", { name: "Increment" });
-    expect(incrementButton).toBeInTheDocument();
+    const result = INITIAL_COUNT - 2;
 
-    const decrementButton = screen.getByRole("button", { name: "Decrement" });
-    expect(decrementButton).toBeInTheDocument();
+    await waitFor(async () => {
+      const incrementButton = screen.getByRole("button", { name: "Increment" });
+      expect(incrementButton).toBeInTheDocument();
 
-    const user = userEvent.setup();
+      const decrementButton = screen.getByRole("button", { name: "Decrement" });
+      expect(decrementButton).toBeInTheDocument();
 
-    await user.click(incrementButton); // +1
-    await user.dblClick(incrementButton); // +3
-    await user.click(decrementButton); // +2
-    await user.dblClick(incrementButton); // +4
-    await user.dblClick(incrementButton); // +6
-    await user.dblClick(decrementButton); // +4
-    await user.dblClick(decrementButton); // +2
-    await user.dblClick(decrementButton); // 0
-    await user.dblClick(decrementButton); // -2
+      const user = userEvent.setup();
 
-    const heading = screen.getByRole("heading");
-    expect(heading).toHaveTextContent("The count is: -2");
+      await user.click(incrementButton); // +1
+      await user.dblClick(incrementButton); // +3
+      await user.click(decrementButton); // +2
+      await user.dblClick(incrementButton); // +4
+      await user.dblClick(incrementButton); // +6
+      await user.dblClick(decrementButton); // +4
+      await user.dblClick(decrementButton); // +2
+      await user.dblClick(decrementButton); // 0
+      await user.dblClick(decrementButton); // -2
+
+      const heading = screen.getByRole("heading");
+      expect(heading).toHaveTextContent(`The count is: ${result}`);
+    });
   });
 });
