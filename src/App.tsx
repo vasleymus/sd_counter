abcc49c0b85dcb4c7c710f41f740efe35a0e54b0
Vasley <vasleymus@gmail.com>
2024-10-25 12:29:52 +0400

    Add 'Add Counter' button

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
+import { useState, Fragment, useRef } from "react";
 import Counter from "./Counter";
 import "./App.css";
 
+type Counters = {
+  id: number;
+  element: JSX.Element;
+}[];
+
 function App() {
-  return <Counter />;
+  const [counters, setCounters] = useState<Counters>([
+    { id: 1, element: <Counter /> },
+  ]);
+  const nextId = useRef(1); // store counter id
+
+  const handleAddCounter = () => {
+    const id = ++nextId.current; // increment id
+    const newCounter = { id, element: <Counter /> };
+    setCounters((c) => [...c, newCounter]);
+  };
+
+  return (
+    <>
+      {counters.map((counter) => {
+        return <Fragment key={counter.id}>{counter.element}</Fragment>;
+      })}
+      <button onClick={handleAddCounter}>Add Counter</button>
+    </>
+  );
 }
 
 export default App;
